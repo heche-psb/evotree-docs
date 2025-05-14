667a7ba759166827f8d7a5c867742bbaf12b9142
heche-psb <heche@HengchiChen>
2025-05-14 17:16:41 +0200

    Priliminary update

---
+++ docs/source/Example_Code/add_timescale.py
@@ -0,0 +1,23 @@
+from evotree.basicdraw import plottree
+TB,tree_object = plottree(tree="FigTree_newick")
+TB.plotnodeuncertainty = True
+# Plot the node uncertainty (usually 95%HPD time uncertainty)
+TB.nulw = 3
+TB.nuccolor = 'gray'
+# Set the linewidth of the node uncertainty band and color
+TB.basicdraw()
+#
+#
+fossilnodes=[('Prasinoderma_coloniale','Amborella_trichopoda'),('Ostreococcus_lucimarinus','Pedinomonas_minor'),('Pedinomonas_minor','Mesostigma_viride'),('Botryococcus_braunii','Volvox_carteri'),('Botryococcus_braunii','Coccomyxa_subellipsoidea'),('Spirogloea_muscicola','Amborella_trichopoda'),('Anthoceros_angustus','Amborella_trichopoda'),('Takakia_lepidozioides','Marchantia_polymorpha'),('Selaginella_moellendorffii','Amborella_trichopoda'),('Adiantum_capillus-veneris','Amborella_trichopoda'),('Cycas_panzhihuaensis','Amborella_trichopoda'),('Aristolochia_fimbriata','Amborella_trichopoda')]
+TB.highlightnode(nodes=fossilnodes,colors=['orange' for i in fossilnodes],nodesizes=[8 for i in fossilnodes],addlegend=True,legendlabel="Fossil calibrations")
+# Highlight certain nodes, set color, size, legend and legendlabel
+#
+#
+TB.highlightclade(clades=[('Amborella_trichopoda','Anthoceros_angustus'),('Zygnema_circumcarinatum_SAG_698-1b','Mesostigma_viride')],facecolors=['red','green'],gradual=True,alphas=[0.6,0.3],rightoffset=0.01,topoffset=0.02,bottomoffset=-0.01,labels=['Embryophyta','Streptophyta'],labelboxcolors=['black','black'],labelcolors=['white','white'])
+TB.highlightclade(clades=[('Volvox_carteri','Pedinomonas_minor'),('Micromonas_pusilla','Ostreococcus_lucimarinus')],facecolors=['gray','black'],gradual=True,alphas=[0.3,0.3],rightoffset=0.01,topoffset=0.02,bottomoffset=-0.01,labels=['Chlorophytina','Prasinophytina'],labelboxcolors=['black','black'],labelcolors=['white','white'])
+# Highlight the MRCA clades, set shadow color, transparency, gradual style, xoffset and yoffset, label texts, labelxoffset and labelyoffset, colors and text box colors
+#
+#
+TB.drawscale(plotfulllengthscale=True,fullscaletickheight=0.1,fullscaleticklabeloffset=0.1,addgeo=True,geoscaling=100,fullscalexticks=[int(i*100) for i in range(14)])
+# Add time-scale at the bottom, set geoscaling (here the branch length is at the unit of 100 million years so we set as 100), preset xticks (by default six evenly-spaced xticks) as [0,100,200,..,1300], yoffset for tick and ticklabel
+TB.saveplot('Add_TimeScale.svg')
--- docs/source/usage.rst
+++ docs/source/usage.rst
@@ -94,4 +94,30 @@ Or using the command below:
 .. image:: Example_Data/Highlight_Clade.svg
 
 
+Adding a time-scale can be achieved using the command below:
+
+>>> from evotree.basicdraw import plottree
+>>> TB,tree_object = plottree(tree="FigTree_newick")
+>>> TB.plotnodeuncertainty = True
+>>> TB.nulw = 3
+>>> TB.nuccolor = 'gray'
+>>> TB.basicdraw()
+>>> fossilnodes=[('Prasinoderma_coloniale','Amborella_trichopoda'),('Ostreococcus_lucimarinus','Pedinomonas_minor'),('Pedinomonas_minor','Mesostigma_viride'),('Botryococcus_braunii','Volvox_carteri'),('Botryococcus_braunii','Coccomyxa_subellipsoidea'),('Spirogloea_muscicola','Amborella_trichopoda'),('Anthoceros_angustus','Amborella_trichopoda'),('Takakia_lepidozioides','Marchantia_polymorpha'),('Selaginella_moellendorffii','Amborella_trichopoda'),('Adiantum_capillus-veneris','Amborella_trichopoda'),('Cycas_panzhihuaensis','Amborella_trichopoda'),('Aristolochia_fimbriata','Amborella_trichopoda')]
+>>> TB.highlightnode(nodes=fossilnodes,colors=['orange' for i in fossilnodes],nodesizes=[8 for i in fossilnodes],addlegend=True,legendlabel="Fossil calibrations")
+>>> TB.highlightclade(clades=[('Amborella_trichopoda','Anthoceros_angustus'),('Zygnema_circumcarinatum_SAG_698-1b','Mesostigma_viride')],facecolors=['red','green'],gradual=True,alphas=[0.6,0.3],rightoffset=0.01,topoffset=0.02,bottomoffset=-0.01,labels=['Embryophyta','Streptophyta'],labelboxcolors=['black','black'],labelcolors=['white','white'])
+>>> TB.highlightclade(clades=[('Volvox_carteri','Pedinomonas_minor'),('Micromonas_pusilla','Ostreococcus_lucimarinus')],facecolors=['gray','black'],gradual=True,alphas=[0.3,0.3],rightoffset=0.01,topoffset=0.02,bottomoffset=-0.01,labels=['Chlorophytina','Prasinophytina'],labelboxcolors=['black','black'],labelcolors=['white','white'])
+>>> TB.drawscale(plotfulllengthscale=True,fullscaletickheight=0.1,fullscaleticklabeloffset=0.1,addgeo=True,geoscaling=100,fullscalexticks=[int(i*100) for i in range(14)])
+>>> TB.saveplot('Add_TimeScale.svg')
+
+Or using the command below:
+
+.. code-block:: console
+
+      (ENV)$ python Example_Code/add_timescale.py
+
+
+.. image:: Example_Data/Add_TimeScale.svg
+
+
+
 
